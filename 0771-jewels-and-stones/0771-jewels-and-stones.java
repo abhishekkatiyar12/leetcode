3d23236cbdbf8e5cab7d737f599b33a9f97ef027
Abhishek Katiyar <142379104+abhishekkatiyar12@users.noreply.github.com>
2024-01-26 11:56:07 +0530

    Time: 2 ms (17.08%), Space: 41.7 MB (32.00%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -1,20 +1,26 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
         
+        // convertng the jewels string to list
+        
         List<Character> jewellist = new ArrayList<>();
 
         for (char c : jewels.toCharArray()) {
             jewellist.add(c);
         }
 
-       // String stones="aAAbbbb";
+      
+        // convertng the stones string to list
         List<Character> stonesList = new ArrayList<>();
 
         for (char c : stones.toCharArray()) {
             stonesList.add(c);
         }
-        //System.out.println(stonesList);
+       
         int count=0;
+        
+        // checking if stines item is in jewels if contains increase count an reduce i to check the element which comes  in place of i after removinf i
+        
       for(int i=0;i<stonesList.size();i++){
           if(jewellist.contains(stonesList.get(i))){
               count++;
@@ -22,8 +28,7 @@ class Solution {
               i--;
           }
       }
-       // System.out.println(count);
-       // System.out.println(stonesList);
+       
         return count;
     }
 }
