4a84a22a7224d0cc85b838b5deb16067edd445c3
Abhishek Katiyar <142379104+abhishekkatiyar12@users.noreply.github.com>
2024-02-16 15:01:23 +0530

    Time: 8 ms (18.81%), Space: 46.7 MB (69.38%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -4,10 +4,6 @@ class Solution {
             nums[i]=(int)Math.pow(nums[i],2);
         }
         Arrays.sort(nums);
-        for(int i=0;i<nums.length;i++) {
-            System.out.print(nums[i]+" ");
-
-        }
         return nums;
     }
 }
