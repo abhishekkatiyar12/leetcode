749d55fe68a9d57dcae408874fd07f397b301e2b
Abhishek Katiyar <142379104+abhishekkatiyar12@users.noreply.github.com>
2024-03-13 10:01:59 +0530

    Time: 51 ms (68.31%), Space: 48.7 MB (70.02%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.js
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -9,14 +9,13 @@ var filter = function(arr, fn) {
     const myArray = [];
 
     // traversing the array
-    for(var i = 0; i < arr.length; i++) {
-        var number = arr[i];
-        var result = fn(number, i);
-        // Check if the result is truthy
-        if (result) {
-            myArray.push(arr[i]);
+    for(var i=0;i<arr.length;i++){
+        var number=arr[i];
+        var result=fn(number,i);
+        if(result){
+        myArray.push(arr[i]);
         }
     }
     return myArray;
-};
 
+};
