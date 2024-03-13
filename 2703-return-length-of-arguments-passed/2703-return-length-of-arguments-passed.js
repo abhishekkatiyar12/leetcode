49302621e04eca6cdfa655321e9430fa17bc25b0
Abhishek Katiyar <142379104+abhishekkatiyar12@users.noreply.github.com>
2024-03-13 10:25:39 +0530

    Time: 43 ms (92.50%), Space: 48.8 MB (39.91%) - LeetHub

---
+++ 2703-return-length-of-arguments-passed/2703-return-length-of-arguments-passed.js
@@ -0,0 +1,15 @@
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    var count =0;
+    for(var i=0;i<args.length;i++){
+        count++;
+    }
+    return count;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
