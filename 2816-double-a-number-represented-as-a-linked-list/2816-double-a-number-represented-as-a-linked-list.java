60b28db2b56bbe29c711209c3b7ebe9daca5f156
Abhishek Katiyar <142379104+abhishekkatiyar12@users.noreply.github.com>
2024-05-07 17:22:08 +0530

    Time: 5 ms (51.55%), Space: 45.8 MB (74.11%) - LeetHub

---
+++ 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.java
@@ -0,0 +1,73 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode doubleIt(ListNode head) {
+        
+       ListNode newHead=reverse(head);
+        ListNode result=doubleList(newHead);
+        return result;
+        
+        
+    }
+    
+    
+    
+    public ListNode reverse(ListNode Head){
+        ListNode curr,next,prev;
+        prev=null;
+        curr=Head;
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+    
+    
+    public ListNode doubleList(ListNode head){
+         ListNode curr,newlist;
+         curr=head;
+        
+        
+         ListNode result=new ListNode();
+        newlist=result;
+        
+
+        int val=0,carry=0;
+        while(curr!=null){
+            ListNode temp=new ListNode();
+            
+            val=curr.val;
+            val+=val;
+            val+=carry;
+            temp.val=val%10;
+            carry=val/10;
+            curr=curr.next;
+            newlist.next=temp;
+            newlist=temp;
+        }
+        result=result.next;
+        if(carry==1){
+           ListNode r1= reverse(result);
+            ListNode n1=new ListNode();
+            n1.val=carry;
+            n1.next=r1;
+            return n1;
+        }else{
+            return reverse(result);
+        }
+        // return result;
+    }
+   
+    
+}
