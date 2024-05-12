66ba9b4c79669b8b30eb9d9a471aeb35eb3a0c42
Abhishek Katiyar <142379104+abhishekkatiyar12@users.noreply.github.com>
2024-05-12 15:06:16 +0530

    Time: 2 ms (100.00%), Space: 45.3 MB (62.99%) - LeetHub

--- 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.java
+++ 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.java
@@ -1,19 +1,29 @@
+// approach
+// to fill the element in the answer matrix we have to find the 3*3 grid from original grid which is starting from the same row and same column 
+
+
+
 class Solution {
     public int[][] largestLocal(int[][] grid) {
         int m=grid.length;
-        int n=grid[0].length;
-        int [][] answer= new int[m-2][n-2];
-      
         
+        // declaring the answer array
+        int [][] answer= new int[m-2][m-2];
+      
+        // filing the values in the answer array 
         for(int i=0;i<m-2;i++){
  
-        for(int j=0;j<n-2;j++){
+        for(int j=0;j<m-2;j++){
             answer[i][j]=findGreatest(grid,i,j);
         }
             }
         
+        // return array(answer)
         return answer;
     }
+    
+    
+    // helper method to find the greatest among the 3*3 matrix
     
     public static int findGreatest(int[][] arr,int a,int b){
         int max=Integer.MIN_VALUE;
