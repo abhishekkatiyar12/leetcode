02cd544760b5e0eb514cd9f6cd3215628f467e42
Abhishek Katiyar <142379104+abhishekkatiyar12@users.noreply.github.com>
2024-06-04 00:56:50 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (50.19%) - LeetHub

---
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        // Reverse the linked list
+        ListNode NewHead = reverseLinkedList(head);
+        ListNode curr = NewHead;
+        
+       if (n == 1) {
+            NewHead = NewHead.next;  
+       } else {
+            int count = 1;
+            while (curr != null && count < n - 1) {
+                curr = curr.next;
+                count++;
+            }
+            if (curr != null && curr.next != null) {
+                curr.next = curr.next.next;  // Skip the nth node
+            }
+        }
+        
+        // Reverse the list again to restore original order
+        head = reverseLinkedList(NewHead);
+        return head;
+    }
+    
+    public static ListNode reverseLinkedList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        while (current != null) {
+            ListNode next = current.next; 
+            current.next = prev;          
+            prev = current;               
+            current = next;
+        }
+        return prev; 
+    }
+}
