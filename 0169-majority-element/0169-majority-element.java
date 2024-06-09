25852ad305148a172038292340510f367a7151c4
Abhishek Katiyar <142379104+abhishekkatiyar12@users.noreply.github.com>
2024-06-10 00:13:28 +0530

    Time: 16 ms (17.42%), Space: 49.5 MB (74.42%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,23 +1,43 @@
-class Solution {
-    public int majorityElement(int[] nums) {
-        int candidate = nums[0];
-        int count = 1;
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         int candidate = nums[0];
+//         int count = 1;
 
        
-        for (int i = 1; i < nums.length; i++) {
-            if (count == 0) {
-                candidate = nums[i];
-                count = 1;
-            } else if (nums[i] == candidate) {
-                count++;
-            } else {
-                count--;
-            }
-        }
+//         for (int i = 1; i < nums.length; i++) {
+//             if (count == 0) {
+//                 candidate = nums[i];
+//                 count = 1;
+//             } else if (nums[i] == candidate) {
+//                 count++;
+//             } else {
+//                 count--;
+//             }
+//         }
 
-        return candidate;
-    }
+//         return candidate;
+//     }
 
         
     
+// }
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        // Create a HashMap to store the count of each element
+        Map<Integer, Integer> countMap = new HashMap<>();
+        int majorityCount = nums.length / 2;
+
+        // Iterate through the array and update the count of each element in the HashMap
+        for (int num : nums) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+            // Check if the current element's count exceeds the majority count
+            if (countMap.get(num) > majorityCount) {
+                return num;
+            }
+        }
+
+        // If no majority element is found (though the problem guarantees one), return -1 as a fallback
+        return -1;
+    }
 }
