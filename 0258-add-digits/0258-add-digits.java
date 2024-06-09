c530163feea44ce61f8fb3c5f3aec140df169794
Abhishek Katiyar <142379104+abhishekkatiyar12@users.noreply.github.com>
2024-06-10 00:36:10 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (88.15%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,19 +1,41 @@
+// class Solution {
+//     public int addDigits(int num) {
+//          int result=num;
+        
+//         while(result>9){
+//            result=addInteger(result);
+//         }
+//         return result;
+//     }
+//     public int addInteger(int x){
+//         int sum=0;
+        
+//         while(x>0){
+//             sum+=x%10;
+//             x /=10;
+//         }
+//         return sum;
+//     }
+// }
+
+
+
+
 class Solution {
     public int addDigits(int num) {
-         int result=num;
-        
-        while(result>9){
-           result=addInteger(result);
+        // If the number is less than 10, it is already a single-digit number
+        if (num < 10) {
+            return num;
         }
-        return result;
-    }
-    public int addInteger(int x){
-        int sum=0;
         
-        while(x>0){
-            sum+=x%10;
-            x /=10;
+        // Calculate the sum of digits
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
         }
-        return sum;
+        
+        // Recursively call addDigits on the sum until it becomes a single-digit number
+        return addDigits(sum);
     }
 }
