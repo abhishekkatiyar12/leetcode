3907e0c216dbe503648e681da4855adacb08911e
Abhishek Katiyar <142379104+abhishekkatiyar12@users.noreply.github.com>
2024-07-04 16:11:36 +0530

    Time: 5 ms (84.77%), Space: 81.9 MB (56.71%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
@@ -10,24 +10,19 @@
  */
 class Solution {
     public ListNode mergeNodes(ListNode head) {
-        ListNode result = new ListNode();
-        ListNode A = result;
-        int temp = 0;
-
-        // Start from head.next to skip the leading zero
-        head = head.next;
-
-        while (head != null) {
-            if (head.val != 0) {
-                temp += head.val; // Accumulate values until we hit a zero
-            } else {
-                A.next = new ListNode(temp); // Create a new node with the accumulated value
-                A = A.next; // Move to the new node
-                temp = 0; // Reset temp for the next segment
+        ListNode result=new ListNode();
+        ListNode A=result;
+        int temp=0;
+        head=head.next;
+        while(head!=null){
+        temp+=head.val;
+            if(head.val==0){
+                A.next= new ListNode(temp);
+                A=A.next;
+                temp=0;
             }
-            head = head.next; // Move to the next node
+            head=head.next;
         }
-
-        return result.next; // Skip the dummy node
+        return result.next;
     }
 }
